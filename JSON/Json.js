337c5dbc674007f44f8f1d00038c5312eab1bc14
Rusiru7 <rusirurathnasiri9@gmail.com>
2025-03-24 22:28:29 +0530

    Json Files usages

---
+++ JSON/Json.js
@@ -0,0 +1,27 @@
+//Json files
+//{key : value}
+
+ // Defining a JSON-like object (student)
+let student = {
+    regNum: '2021/ict/14',  // Registration number
+    Name: 'Yasas',  // Student name
+    Age: 21,  // Age of the student
+    course: 'IT',  // Course enrolled
+    skilled: ['Java', 'JS', 'C++']  // List of skills
+};
+
+
+console.log(student);
+
+// Accessing a specific property
+ console.log(student.Name); 
+
+// Defining an array of student objects
+let students = [
+    { regNo: '2021/ict/01', Name: 'Yas', Age: 21, Course: 'IT' },
+    { regNo: '2021/ict/02', Name: 'Las', Age: 23, Course: 'IT' },
+    { regNo: '2021/ict/03', Name: 'Att', Age: 21, Course: 'IT' }
+];
+
+
+console.log(students);
